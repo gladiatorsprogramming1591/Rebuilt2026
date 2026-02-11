29a67c2b4d0c2549ad20e021d6ccc4402339fc60
Kiley <kileybroyld@gmail.com>
2026-02-10 20:33:31 -0500

    Tuned straight-line path +/- 0.5in
    Tuned PathPlanner (PP) PID constants, corrected TunerConstants module offsets, updated PP robot config, and slowed Max Vel & Accel globally in PP.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -145,8 +145,7 @@ public class RobotContainer {
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
     // Set up SysId routines
-    autoChooser.addOption(
-        "Straight Line", new PathPlannerAuto("Straight ln auto"));
+    autoChooser.addOption("Straight Line", new PathPlannerAuto("Straight ln auto"));
     autoChooser.addOption(
         "Drive Wheel Radius Characterization", DriveCommands.wheelRadiusCharacterization(drive));
     autoChooser.addOption(
