3a9e47a473e482500554429801f9f12d1ddc5d5e
Driverstation 137 <gladiatorsprogramming1591@gmail.com>
2026-03-10 20:36:20 -0400

    Moved to Motion Magic Vel control for shooter.
    Removed kS and limited acceleration.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DataLogManager;
--- src/main/java/frc/robot/subsystems/shooter/ShooterIOKraken.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOKraken.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
@@ -16,7 +17,9 @@ import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
 
 public class ShooterIOKraken implements ShooterIO {
-  private final VelocityVoltage velocityControl = new VelocityVoltage(0).withVelocity(0.0);
+  private final VelocityVoltage velocityControl = new VelocityVoltage(0).withSlot(0);
+  final MotionMagicVelocityVoltage magicVelocityControl =
+      new MotionMagicVelocityVoltage(0).withSlot(0);
 
   private final StatusSignal<AngularVelocity> shooterRPS;
   private final StatusSignal<Voltage> shooterAppliedVolts;
@@ -53,6 +56,21 @@ public class ShooterIOKraken implements ShooterIO {
     rightConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
     rightConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
 
+    var slot0Configs = rightConfig.Slot0;
+
+    slot0Configs.kS = 0.0; // Add 0.0 V output to overcome static friction
+    slot0Configs.kV = 0.125; // A velocity target of 1 rps results in 0.125 V output
+    slot0Configs.kA = 0.01; // An acceleration of 1 rps/s requires 0.01 V output
+    slot0Configs.kP = 0.65; // An error of 1 rps results in 0.65 V output
+    slot0Configs.kI = 0; // no output for integrated error
+    slot0Configs.kD = 0; // no output for error derivative
+
+    // set Motion Magic Velocity settings
+    var motionMagicConfigs = rightConfig.MotionMagic;
+    motionMagicConfigs.MotionMagicAcceleration =
+        200; // Target acceleration of 200 rps/s (0.50 (?) seconds to max)
+    motionMagicConfigs.MotionMagicJerk = 2000; // Target jerk of 2000 rps/s/s (0.2 (?) seconds)
+
     // tryUntilOk(5, ()-> shooterFollower.getConfigurator().apply(shooterConfig, 0.25));
     var leftConfig = rightConfig.clone();
 
@@ -63,15 +81,6 @@ public class ShooterIOKraken implements ShooterIO {
         new Follower(rightShooterLeader.getDeviceID(), MotorAlignmentValue.Aligned));
     leftShooterFollower.setControl(
         new Follower(leftShooterLeader.getDeviceID(), MotorAlignmentValue.Aligned));
-
-    var slot0Configs = new Slot0Configs();
-    slot0Configs.kP = ShooterConstants.kP;
-    slot0Configs.kI = ShooterConstants.kI;
-    slot0Configs.kD = ShooterConstants.kD;
-    slot0Configs.kV = ShooterConstants.kV;
-
-    rightShooterLeader.getConfigurator().apply(slot0Configs);
-    leftShooterLeader.getConfigurator().apply(slot0Configs);
   }
 
   @Override
@@ -104,11 +113,15 @@ public class ShooterIOKraken implements ShooterIO {
     slot0Configs.kD = outputs.kD;
     slot0Configs.kV = outputs.kV;
 
-    // rightShooterLeader.getConfigurator().apply(slot0Configs);
+    // rightShooterLeader.getConfigurator().apply(slot0Configs); // Cannot call periodicly
     // leftShooterLeader.getConfigurator().apply(slot0Configs);
 
-    rightShooterLeader.setControl(velocityControl.withVelocity(outputs.desiredVelocityRPM / 60));
-    leftShooterLeader.setControl(velocityControl.withVelocity(outputs.desiredVelocityRPM / 60));
+    // rightShooterLeader.setControl(velocityControl.withVelocity(outputs.desiredVelocityRPM / 60));
+    // leftShooterLeader.setControl(velocityControl.withVelocity(outputs.desiredVelocityRPM / 60));
+    rightShooterLeader.setControl(
+        magicVelocityControl.withVelocity(outputs.desiredVelocityRPM / 60));
+    leftShooterLeader.setControl(
+        magicVelocityControl.withVelocity(outputs.desiredVelocityRPM / 60));
   }
 
   @Override
