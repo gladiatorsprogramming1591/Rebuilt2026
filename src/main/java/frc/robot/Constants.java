828f389ac69547acebad624f06b43d0eb4592a88
DS 123 (Primary) <gladiatorsprogramming1591@gmail.com>
2026-05-31 09:50:31 -0400

    Replay and autoformatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,9 +16,9 @@ import edu.wpi.first.wpilibj.RobotBase;
  * (log replay from a file).
  */
 public final class Constants {
-  public static final Mode simMode = Mode.SIM;
+  public static final Mode simMode = Mode.REPLAY;
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
-  public static final boolean tuningMode = false; // TODO: Remove before Tech Valley
+  public static final boolean tuningMode = false;
   public static final boolean disableHAL = false;
 
   public static final class Tuning {
